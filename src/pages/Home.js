b9f82e86f5a83c47b2370385285d255e66ba7586
Vishnuads <your_email@example.com>
2025-09-01 15:37:14 +0530

    Updated: bug fix and UI change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -260,7 +260,7 @@ const settings = {
                             <div className="flex flex-col items-center gap-y-3">
                               {/* Image */}
                               <img
-                                src={`${API_BASE}${item.image}`}
+                                src={item.imageUrl}
                                 className="w-14 md:w-20 object-contain mb-2 filter brightness-0"
                                 alt={item.titleLine}
                                 loading="lazy"
