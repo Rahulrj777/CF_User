43094fdd8baf73d229829ce0aca623912d5c3632
Vishnuads <your_email@example.com>
2025-09-12 10:01:37 +0530

    Updated: bug fix and UI change

--- src/pages/VideoGallery.jsx
+++ src/pages/VideoGallery.jsx
@@ -1,37 +1,17 @@
 // pages/VideoGallery.jsx
 import { Link } from "react-router-dom";
-import img1 from "../images/gust.jpg"
-import img2 from "../images/newlook.jpg"
-import img3 from "../images/highlights.jpg"
-import img4 from "../images/webinar.jpg"
-import img5 from "../images/review.jpg"
+import vid1 from "../images/Video/🎬 Learn Screenplay & Direction from Master Filmmaker P.S. VinothrajInternationally Acclaimed Di.mp4";
+import vid2 from "../images/Video/🎬 Cinema Factory Academy – Special Session✨ Showcasing Flex Bot 2.0 – Motion Control Robotic Ar.mp4";
+import vid3 from "../images/Video/Cyber Gaffing in India for Virtual Production🎬 Cyber Gaffer – Virtual Production Module at Stag.mp4";
+import vid4 from "../images/Video/“Vehicle Rigging – A Unique Training at Cinema Factory Academy”At CFA, students learn vehicle ri.mp4";
+import vid5 from "../images/Video/Spot frame composition and shot design. with Flexbot 2.0 robotic arm and red komodo 🎥🔥Shot des.mp4";
 
 const categories = [
-  {
-    name: "Guest Lecture",
-    slug: "guest-lecture",
-    thumb: img1,
-  },
-  {
-    name: "Highlights",
-    slug: "highlights",
-    thumb: img3,
-  },
-  {
-    name: "New Launches",
-    slug: "new-launches",
-    thumb: img2,
-  },
-  {
-    name: "Review",
-    slug: "review",
-    thumb: img5,
-  },
-  {
-    name: "Student Works",
-    slug: "student-works",
-    thumb: img4,
-  },
+  { name: "Guest Lecture", slug: "guest-lecture", thumb: vid1 },
+  { name: "Highlights", slug: "highlights", thumb: vid3 },
+  { name: "New Launches", slug: "new-launches", thumb: vid2 },
+  { name: "Review", slug: "review", thumb: vid5 },
+  { name: "Student Works", slug: "student-works", thumb: vid4 },
 ];
 
 const VideoGallery = () => {
@@ -47,11 +27,15 @@ const VideoGallery = () => {
             className="bg-gray-900 rounded-xl overflow-hidden hover:shadow-2xl transition-all duration-300 cursor-pointer transform hover:scale-105"
           >
             <div className="relative">
-              <img
+              <video
                 src={cat.thumb}
-                alt={cat.name}
                 className="w-full h-80 object-cover"
+                muted
+                autoPlay
+                loop
+                playsInline
               />
+
               <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity">
                 <span className="text-white text-xl font-semibold">
                   {cat.name}
