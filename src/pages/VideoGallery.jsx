b78dd9342e2d0a18855017927af5427b890adbef
Vishnuads <your_email@example.com>
2025-09-13 11:21:07 +0530

    Updated: bug fix and UI change

--- src/pages/VideoGallery.jsx
+++ src/pages/VideoGallery.jsx
@@ -86,12 +86,12 @@ const VideoGallery = () => {
                   {/* Animated overlay */}
                   <div
                     className="
-      absolute bottom-0 left-0 w-full 
-      bg-black/50 text-center py-2
-      translate-y-4 opacity-0
-      group-hover:translate-y-0 group-hover:opacity-100
-      transition-all duration-500
-    "
+                      absolute bottom-10 left-0 w-full 
+                      bg-black/50 text-center py-2
+                      translate-y-4 opacity-0
+                      group-hover:translate-y-0 group-hover:opacity-100
+                      transition-all duration-500
+                    "
                   >
                     <span className="text-white font-semibold text-lg tracking-wide">
                       {video.title || video.category}
